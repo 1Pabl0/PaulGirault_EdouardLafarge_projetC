d19f89b5cc64b20827cb662e0ad4a27cae94d15d
paulgirault <paul.girault@efrei.net>
2025-03-20 18:37:57 +0100

    commit

--- bmp8_niveau_de_gris.c
+++ bmp8_niveau_de_gris.c
@@ -1,5 +1,3 @@
-//
-// Created by Edouard LAFARGE on 20/03/2025.
-//
-
 #include "bmp8_niveau_de_gris.h"
+
+
--- bmp8_niveau_de_gris.h
+++ bmp8_niveau_de_gris.h
@@ -1,8 +1,18 @@
-//
-// Created by Edouard LAFARGE on 20/03/2025.
-//
-
 #ifndef PAULGIRAULT_EDOUARDLAFARGE_PROJETC_BMP8_NIVEAU_DE_GRIS_H
 #define PAULGIRAULT_EDOUARDLAFARGE_PROJETC_BMP8_NIVEAU_DE_GRIS_H
+
+typedef struct {
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
+} t_bmp8;
+
+
+
+
 
 #endif //PAULGIRAULT_EDOUARDLAFARGE_PROJETC_BMP8_NIVEAU_DE_GRIS_H
