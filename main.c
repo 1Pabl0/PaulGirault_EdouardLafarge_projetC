36b198709b674f170bac6adbd4ab9904dc8135f6
paulgirault <paul.girault@efrei.net>
2025-03-20 18:13:24 +0100

    commit

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "menu.h"
 
 int main() {
-    printf("Hello, World!\n");
+    Menu();
     return 0;
 }
--- menu.c
+++ menu.c
@@ -1,5 +1,18 @@
-//
-// Created by Edouard LAFARGE on 20/03/2025.
-//
-
+#include <stdio.h>
 #include "menu.h"
+
+int Menu(){
+    int nombre=0;
+    printf("Veuillez choisir une option : \n");
+    printf("1. Ouvrir une image\n");
+    printf("2; Appliquer un filtre\n");
+    printf("4. Afficher les informations de l'image\n");
+    printf("5. Quitter\n");
+    scanf("%d",&nombre);
+    printf("Votre choix : %d",nombre);
+
+    return nombre;
+}
+
+
+
