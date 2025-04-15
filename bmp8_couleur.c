05baa66101143bf1f4f155ff78d7d32f549f8a91
edouardlafarge <edouardlafarge@gmail.com>
2025-04-15 19:45:15 +0200

    commit

--- bmp8_couleur.c
+++ bmp8_couleur.c
@@ -4,7 +4,3 @@
 #include <math.h>
 
 
-t_pixel ** bmp24_allocateDataPixels (int width, int height);
-void bmp24_freeDataPixels (t_pixel ** pixels, int height);
-t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
-void bmp24_free (t_bmp24 * img);
--- bmp8_couleur.h
+++ bmp8_couleur.h
@@ -63,8 +63,5 @@ void bmp24_freeDataPixels (t_pixel ** pixels, int height);
 t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
 void bmp24_free (t_bmp24 * img);
 
-t_pixel ** bmp24_allocateDataPixels (int width, int height);
-void bmp24_freeDataPixels (t_pixel ** pixels, int height);
-t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
-void bmp24_free (t_bmp24 * img);
+
 #endif //PAULGIRAULT_EDOUARDLAFARGE_PROJETC_BMP8_COULEUR_H
